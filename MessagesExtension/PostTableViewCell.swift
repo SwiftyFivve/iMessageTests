a43f62008de3f792e37160f3238e1bdc80af7769
maybeimyellow <jweav6@fullsail.edu>
2016-12-21 14:53:57 -0500

    Initial Commit

---
+++ MessagesExtension/PostTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostTableViewCell.swift
+//  RedditMessagesTestApp
+//
+//  Created by Jordan on 12/20/16.
+//  Copyright © 2016 Jordan. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var urlLabel: UILabel!
+    
+
+}
